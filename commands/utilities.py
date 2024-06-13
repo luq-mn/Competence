4b822312f930ebc60fbdb4bb79d498d08262a6eb
luq <luqman.yazrie@outlook.com>
2024-06-13 10:08:59 +0800

    new requirements, get user info command

--- commands/utilities.py
+++ commands/utilities.py
@@ -1,4 +1,4 @@
-import discord
+import discord, datetime
 from discord import ApplicationContext, Option, Embed
 from discord.ext import commands
 from discord.ext.commands import Cog
@@ -10,7 +10,37 @@ stats = StatisticsTracker()
 class Utilities(Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+
+        # User command group
+        user = bot.create_group("user", "Commands related to a user")
+
+        @user.command(
+            name= "info",
+            description= "Get info of a user"
+        )
+        async def user_info(ctx, member: Option(discord.User, description= "User to get info of", required= False)): # type: ignore
+            if member == None:
+                member = ctx.author
+            
+            await ctx.respond(
+                embed= Embed(
+                title= member.name,
+                color= discord.Color.green(),
+                description= "User info:"
+                )
+                .add_field(name= "Nickname", value= member.display_name, inline= True)
+                .add_field(name= "ID", value= member.id, inline= True)
+                .add_field(name= "\u200b", value= "\u200b")
+                .add_field(name= "Joined", value= member.joined_at, inline= True)
+                .add_field(name= "Created", value= member.created_at, inline= True)
+                .add_field(name= "\u200b", value= "\u200b")
+                .add_field(name= "Roles", value= len(member.roles), inline= True)
+                .add_field(name= "Status", value= member.status, inline= True)
+
+                .set_footer(text= f"Invoked by {ctx.author.name}")
+            )
+            stats.command_invoked(ctx.author.id, ctx.guild.id, "user_info", f"{member.display_name, member.id, member.joined_at, member.created_at, len(member.roles), member.status, member.bot}")
+
     # Ping
     @discord.slash_command(
         name= "ping",
