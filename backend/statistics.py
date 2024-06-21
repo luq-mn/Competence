700219b35475a4a37aa22019c0dae02225d6147c
luq <luqman.yazrie@outlook.com>
2024-06-22 02:25:33 +0800

    Statistics overhaul

--- backend/statistics.py
+++ backend/statistics.py
@@ -1,31 +1,44 @@
-import sqlite3
+import sqlite3, os, datetime
 from main import get_datetime
+
+def get_datetime():
+    return datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
 
 class StatisticsTracker:
     def __init__(self):
-        self.db_name = "backend/database/statistics.db"
+        self.db_name = f"{os.getcwd()}/database/statistics.db"
 
         # Create table if it does not exist
         with sqlite3.connect(self.db_name) as conn:
             with conn:
-                conn.execute("CREATE TABLE IF NOT EXISTS commands (user_id INTEGER, command TEXT, output TEXT, timestamp TEXT)")
-                conn.execute("CREATE TABLE IF NOT EXISTS transactions (sender_id INTEGER, receiver_id INTEGER, amount INTEGER, note TEXT, timestamp TEXT)")
+                conn.execute("CREATE TABLE IF NOT EXISTS transactions (sender_id INTEGER, recipient_id INTEGER, amount INTEGER, note TEXT, timestamp TEXT)")
+                conn.execute("CREATE TABLE IF NOT EXISTS commands (command TEXT, user_id INTEGER, latency INTEGER, timestamp TEXT)")
+                conn.execute("CREATE TABLE IF NOT EXISTS bot (action TEXT, details TEXT, timestamp TEXT)")
                 conn.commit()
     
-    # Log invoked commands
-    def command_log(self, user_id, command, output):
+    # Log bot actions and master commands
+    def bot_log(self, action, details):
         with sqlite3.connect(self.db_name) as conn:
             with conn:
-                conn.execute("INSERT INTO commands (user_id, command, output, timestamp) VALUES (?, ?, ?, ?)", (user_id, command, output, get_datetime()))
+                conn.execute("INSERT INTO bot (action, details, timestamp) VALUES (?, ?, ?)", (action, details, get_datetime()))
                 conn.commit()
 
-                print(f"Command '{command}' invoked by user {user_id} at {get_datetime()}\nOutput: {output}")
+                print(f"Bot action {action} with details {details} at {get_datetime()}")
+
+    # Log invoked commands
+    def command_log(self, user_id, command, latency):
+        with sqlite3.connect(self.db_name) as conn:
+            with conn:
+                conn.execute("INSERT INTO commands (user_id, command, latency, timestamp) VALUES (?, ?, ?, ?)", (user_id, command, latency, get_datetime()))
+                conn.commit()
+
+                print(f"Command {command} invoked by {user_id} at {get_datetime()}")
     
     # Log transactions
-    def transaction_log(self, sender_id, receiver_id, amount, note):
+    def transaction_log(self, sender_id, recipient_id, amount, note):
         with sqlite3.connect(self.db_name) as conn:
             with conn:
-                conn.execute("INSERT INTO transactions (sender_id, receiver_id, amount, note, timestamp) VALUES (?, ?, ?, ?, ?)", (sender_id, receiver_id, amount, note, get_datetime()))
+                conn.execute("INSERT INTO transactions (sender_id, recipient_id, amount, note, timestamp) VALUES (?, ?, ?, ?, ?)", (sender_id, recipient_id, amount, note, get_datetime()))
                 conn.commit()
 
-                print(f"Transaction from {sender_id} to {receiver_id} of {amount} at {get_datetime()}")
+                print(f"Transaction from {sender_id} to {recipient_id} of {amount} at {get_datetime()}")
