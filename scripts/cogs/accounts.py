1d980390920749dab40b40092bbfab9820960e83
opo <169742991+oponic@users.noreply.github.com>
2024-06-27 21:57:51 -0500

    Update accounts.py

--- scripts/cogs/accounts.py
+++ scripts/cogs/accounts.py
@@ -3,15 +3,15 @@ from discord.ext import commands
 import scripts.embeds as embeds
 
 class Accounts(commands.Cog):
-    @discord.slash_command(name = "account", description = "Overview of your account")
+    @discord.slash_command(name = "account", description = "Account Overview")
     async def account(self, ctx):
         pass
 
-    @discord.slash_command(name = "lock", description = "Toggle ability to perform actions on your account")
+    @discord.slash_command(name = "lock", description = "Toggle Account Usability")
     async def balance(self, ctx):
         pass
 
-    @discord.slash_command(name = "settings", description = "Settings for your account")
+    @discord.slash_command(name = "settings", description = "Account Settings")
     async def balance(self, ctx):
         pass
 
