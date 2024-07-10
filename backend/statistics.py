2ebbae75ca35c599555c8653f70261a3deba21d4
luq-mn <luqman.yazrie@outlook.com>
2024-07-11 01:49:10 +0800

    Updated statistics backend

--- backend/statistics.py
+++ backend/statistics.py
@@ -9,35 +9,42 @@ class StatisticsTracker:
 
         # Create table if it does not exist
         with sqlite3.connect(self.db_name) as conn:
+            # with conn:
+            #     conn.execute("CREATE TABLE IF NOT EXISTS transactions (sender_id INTEGER, recipient_id INTEGER, amount INTEGER, note TEXT, timestamp TEXT)")
+            #     conn.execute("CREATE TABLE IF NOT EXISTS commands (command TEXT, user_id INTEGER, latency INTEGER, timestamp TEXT)")
+            #     conn.execute("CREATE TABLE IF NOT EXISTS bot (action TEXT, details TEXT, timestamp TEXT)")
+            #     conn.commit()
             with conn:
-                conn.execute("CREATE TABLE IF NOT EXISTS transactions (sender_id INTEGER, recipient_id INTEGER, amount INTEGER, note TEXT, timestamp TEXT)")
-                conn.execute("CREATE TABLE IF NOT EXISTS commands (command TEXT, user_id INTEGER, latency INTEGER, timestamp TEXT)")
-                conn.execute("CREATE TABLE IF NOT EXISTS bot (action TEXT, details TEXT, timestamp TEXT)")
-                conn.commit()
+                # commands table (author_id, command, output, latency, timestamp)
+                conn.execute("CREATE TABLE IF NOT EXISTS commands (author_id INTEGER, command TEXT, output TEXT, latency INTEGER, timestamp TEXT)")
+                # events table (type, details, timestamp)
+                conn.execute("CREATE TABLE IF NOT EXISTS events (type TEXT, details TEXT, timestamp TEXT)")
+                # transactions table (sender_id, recipient_id, currency, amount, description, timestamp)
+                conn.execute("CREATE TABLE IF NOT EXISTS transactions (sender_id INTEGER, recipient_id INTEGER, currency TEXT, amount FLOAT, description TEXT, timestamp TEXT)")
     
     # Log bot actions and master commands
     def bot_log(self, action, details):
         with sqlite3.connect(self.db_name) as conn:
             with conn:
-                conn.execute("INSERT INTO bot (action, details, timestamp) VALUES (?, ?, ?)", (action, details, get_datetime()))
+                conn.execute("INSERT INTO events (type, details, timestamp) VALUES (?, ?, ?)", (action, details, get_datetime()))
                 conn.commit()
 
                 print(f"-\nBot action [{action}] with details [{details}]\n{get_datetime()}")
 
     # Log invoked commands
-    def command_log(self, user_id, command, latency):
+    def command_log(self, user_id, command, output, latency):
         with sqlite3.connect(self.db_name) as conn:
             with conn:
-                conn.execute("INSERT INTO commands (user_id, command, latency, timestamp) VALUES (?, ?, ?, ?)", (user_id, command, latency, get_datetime()))
+                conn.execute("INSERT INTO commands (author_id, command, output, latency, timestamp) VALUES (?, ?, ?, ?, ?)", (user_id, command, output, latency, get_datetime()))
                 conn.commit()
 
                 print(f"-\nCommand '{command}' invoked by [{user_id}]\n{get_datetime()} | Latency: {latency}ms")
     
     # Log transactions
-    def transaction_log(self, sender_id, recipient_id, amount, note):
+    def transaction_log(self, sender_id, recipient_id, currency, amount, description):
         with sqlite3.connect(self.db_name) as conn:
             with conn:
-                conn.execute("INSERT INTO transactions (sender_id, recipient_id, amount, note, timestamp) VALUES (?, ?, ?, ?, ?)", (sender_id, recipient_id, amount, note, get_datetime()))
+                conn.execute("INSERT INTO transactions (sender_id, recipient_id, currency, amount, description, timestamp) VALUES (?, ?, ?, ?, ?, ?)", (sender_id, recipient_id, currency, amount, description, get_datetime()))
                 conn.commit()
 
-                print(f"-\nTransaction from [{sender_id}] to [{recipient_id}] of [{amount}]\n{get_datetime()}")
+                print(f"-\nTransaction from [{sender_id}] to [{recipient_id}] of [{amount}] in {currency}\n{get_datetime()}")
